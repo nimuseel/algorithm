66c021f3c65413e6980378b95b199f3bf66e2b76
Sumin Lee <48236404+nimuseel@users.noreply.github.com>
2025-11-20 10:58:32 +0900

    Title: 세 실수의 반올림, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251120/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+console.log(Number(input[0]).toFixed(3));
+console.log(Number(input[1]).toFixed(3));
+console.log(Number(input[2]).toFixed(3));
