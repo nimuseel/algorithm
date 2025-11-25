e27b2fb454e2ee73bc415f3d78548326d73bf865
Sumin Lee <48236404+nimuseel@users.noreply.github.com>
2025-11-25 13:25:24 +0900

    Title: 비교에 따른 연산, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/비교에 따른 연산/operation-based-on-comparison.js
+++ 251125/비교에 따른 연산/operation-based-on-comparison.js
@@ -4,8 +4,14 @@ const input = fs.readFileSync(0).toString().trim().split(' ')
 const a = Number(input[0])
 const b = Number(input[1])
 
-if (a > b) {
-    console.log(a * b)
+// if (a > b) {
+//     console.log(a * b)
+// } else {
+//     console.log(Math.round(a / b))
+// }
+
+if(a > b) {
+  console.log(a * b);
 } else {
-    console.log(a % b)
-}
+  console.log(Math.floor(b / a));
+} 
