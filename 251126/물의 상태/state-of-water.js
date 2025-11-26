c14b9411113346d6ad2438dd1d2b07482f6f7b05
Sumin Lee <48236404+nimuseel@users.noreply.github.com>
2025-11-26 15:37:32 +0900

    Title: 물의 상태, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251126/물의 상태/state-of-water.js
+++ 251126/물의 상태/state-of-water.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const input = Number(fs.readFileSync(0).toString().trim())
 
-if (input <= 0) {
+if (input < 0) {
     console.log("ice")
 } else if (input >= 100) {
     console.log("vapor")
